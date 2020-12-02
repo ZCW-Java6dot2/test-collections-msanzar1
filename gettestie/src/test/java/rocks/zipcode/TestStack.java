bf30f495651f5f859304c8c65b60b5affb9f436a
munir <msanzar87@gmail.com>
2020-12-02 15:54:01 -0500

    incom

--- gettestie/src/test/java/rocks/zipcode/TestStack.java
+++ gettestie/src/test/java/rocks/zipcode/TestStack.java
@@ -1,5 +1,8 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 import java.util.Stack;
 
@@ -19,6 +22,33 @@ public class TestStack {
         stack.push("Hello world");
         assertEquals(false, stack.isEmpty()); // false
     }
+@Test
+    public void TestPop(){
+        //Given
+    Stack<String> stack = new Stack<>();//make new stack
+    String expected = "Hi";//make new string
+    stack.push(expected);//add string to stack
 
+    //when (define what actually happens in this section)
+    String actual = stack.pop();//this is what is actually happening and what we expect from above
+
+    //Then Writing your assertions (if expect and actual are the same)
+    Assert.assertEquals(expected,actual);
+}
+
+@Test
+    public void TestPeek(){
+    //Given
+    Stack<String> stack = new Stack<>();
+    String expected = "Greetings";
+    stack.push(expected);
+
+    //when
+    String actual = stack.peek();
+
+    //Then
+    Assert.assertEquals(expected,actual);
+
+}
     // Make a bigger test exercising more Stack methods.....
 }
